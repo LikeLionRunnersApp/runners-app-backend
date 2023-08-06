fd93c1e0836578e0d0c3bcff3abeec1d98d08d0c
juntae <juntae6942@o.cnu.ac.kr>
2023-08-06 14:18:27 +0900

    member&board domain define+repository

---
+++ src/test/java/likelion/running/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,56 @@
+package likelion.running.domain.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+
+class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+    @Test
+    public void save(){
+        //given
+        Member member = new Member();
+        member.setName("ya ong");
+        member.setMemberId("testMan");
+        member.setEmail("lion@naver.com");
+        member.setPassword("123");
+        //when
+        Member result = memberRepository.save(member);
+
+        //then
+        Assertions.assertThat(result).isEqualTo(member);
+        Assertions.assertThat(result.getName()).isEqualTo(member.getName());
+
+    }
+
+    @Test
+    public void findById(){
+        //given
+        Member member1 = new Member();
+        member1.setName("ya ong");
+        member1.setMemberId("testMan");
+        member1.setEmail("lion@naver.com");
+        member1.setPassword("123");
+
+        Member member2 = new Member();
+        member2.setName("ya ong2");
+        member2.setMemberId("testMan2");
+        member2.setEmail("like@naver.com");
+        member2.setPassword("1234");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        //when
+        Optional<Member> result = memberRepository.findByName(member2.getName());
+
+        //then
+        Assertions.assertThat(result).isPresent();
+        Assertions.assertThat(result.get()).isEqualTo(member1);
+        Assertions.assertThat(result.get()).isEqualTo(member2);
+        Assertions.assertThat(result.get().getName()).isEqualTo(member2.getName());
+        Assertions.assertThat(result.get().getEmail()).isEqualTo(member2.getEmail());
+    }
+
+}
