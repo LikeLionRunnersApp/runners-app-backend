ae84c39b2fd0fccd47fb5b221351f69e45dbb94f
juntae <juntae6942@o.cnu.ac.kr>
2023-08-07 20:05:13 +0900

    Member, Board edit

--- src/main/java/likelion/running/domain/board/Board.java
+++ src/main/java/likelion/running/domain/board/Board.java
@@ -2,13 +2,23 @@ package likelion.running.domain.board;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
 @Data
+@Entity
 public class Board {
 
+    @Id
     private Long id;
     private Long hostId;
 
+    @Column(length = 100)
     private String title;
+
+    @Column(length = 500)
     private String content;
     private String runningType;
 
@@ -18,4 +28,12 @@ public class Board {
     private String runTime;
     private String walkTime;
     private int fullTime;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
